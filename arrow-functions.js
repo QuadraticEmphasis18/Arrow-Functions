16d02ba4e3dcc34fe268b125b85cb9426dc593d1
QuadraticEmphasis18 <lexwardle@gmail.com>
2024-01-03 22:26:02 +0000

    arrow-functions.js

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,17 +4,29 @@
  */
 
 // Vanilla JavaScript Function
-function addTwoNumbers(a, b) {
+// function addTwoNumbers(a, b) {
+    // Code block
+    // return a + b;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
+
+// Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
     // Code block
     return a + b;
 }
 let sum = addTwoNumbers(3, 5);
 console.log(sum);
-
-// Arrow Function With Parameters
-
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b; 
+let sum2 = addTwoNumbers(6,4);
+console.log(sum2); 
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello');
 
 // Returning Multiple Lines
